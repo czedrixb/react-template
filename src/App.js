d85df9e130212675f57f9551a47a4fccc458fc83
czedrixb <czedrixb@gmail.com>
2024-09-10 19:39:27 +0800

    added tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1 className="text-2xl text-red-500 font-bold">App Component</h1>
       </div>
     </div>
   );
